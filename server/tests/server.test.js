a538959ee9992e95e2f51dc56b31322c061af61f
Jason Wen <jason@leadsocial.com>
2017-02-14 11:32:32 +0100

    Setup app for heroku with mongolab

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,13 +1,16 @@
 const expect = require('expect');
 const request = require('supertest');
+const { ObjectId } = require('mongodb');
 
 const { app } = require('./../server.js');
 const { Todo } = require('./../models/todo');
 
 // dummy data
 const todos = [{
+  _id: new ObjectId(),
   text: 'First test todo'
 }, {
+  _id: new ObjectId(),
   text: 'Second test todo'
 }];
 
@@ -71,6 +74,33 @@ describe('POST /todos', () => {
         .expect((response) => {
           expect(response.body.todos.length).toBe(2);
         })
+        .end(done);
+    });
+  });
+
+  describe('GET /todos/:id', () => {
+    it('should return todo doc', (done) => {
+      request(app)
+        .get(`/todos/${todos[0]._id.toHexString()}`)
+        .expect(200)
+        .expect((response) => {
+          expect(response.body.todo.text).toBe(todos[0].text);
+        })
+        .end(done);
+    });
+
+    it('should return a 404 if todo not found', (done) => {
+      const hexObjectId = new ObjectId().toHexString();
+      request(app)
+        .get(`/todos/${hexObjectId}`)
+        .expect(404)
+        .end(done);
+    });
+
+    it('should return 404 for non-object ids', (done) => {
+      request(app)
+        .get('/todos/123')
+        .expect(404)
         .end(done);
     });
   });
