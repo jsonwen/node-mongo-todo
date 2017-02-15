f04c95cfe89c9b7ec0bf7032c089cd34dc58eb4e
Jason Wen <jason@leadsocial.com>
2017-02-15 19:10:54 +0100

    Add validator

--- server/models/user.js
+++ server/models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 const jwt = require('jsonwebtoken');
 const _ = require('lodash');
 const bcrypt = require('bcryptjs');
